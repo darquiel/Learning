a492b58a9a56dc60a19af73be0b6f12d269f5f4e
MjA <vamp_paradox@hotmail.com>
2021-06-18 18:12:00 -0700

    Completed challenge for chapter 5.

--- swift/Ex_Files_Swift_5_EssT/Exercise Files/Ch05/Ch05_Challenge.playground/Pages/Starter.xcplaygroundpage/Contents.swift
+++ swift/Ex_Files_Swift_5_EssT/Exercise Files/Ch05/Ch05_Challenge.playground/Pages/Starter.xcplaygroundpage/Contents.swift
@@ -15,16 +15,37 @@
  
  */
 // 1
+typealias Attack = (name: String, damage: Int)
 
 // 2
+func attackEnemy(damage: Int) {
+    print("Attacking enemy for \(damage) Damage!")
+}
+
+// attackEnemy(damage: 45)
 
 // 3
+func attackEnemy(attack: Attack) -> String {
+    return "Attacked with \(attack.name).\nEnemy hit for \(attack.damage) points for Damage!\n"
+}
 
 // 4
+attackEnemy(damage: 45)
+var message = attackEnemy(attack: ("Sunstrike", 45))
+print(message)
 
 // 5
+typealias AttackClosure = ([Attack]) -> Void
+let playerActions = [("Fire Ball", 165), ("Magic Missile", 35), ("Dragon Claw", 75)]
 
 // 6
+func fetchPlayerAttacks(attacks: AttackClosure) {
+    attacks(playerActions)
+}
 
 // 7
-
+fetchPlayerAttacks { (attackOptions) in
+    for (name, damage) in attackOptions {
+        print("\(name) will hit for \(damage) if cast.")
+    }
+}
